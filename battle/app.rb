acee875ebf6b954e7a2cde4ee57e7522e80cf9ce
Ben <ben.lynch1931@gmail.com>
2020-10-14 09:26:21 +0100

    Transferred from Charlie's repo and initialized all I need to get it to work

---
+++ battle/app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+
+class Battle < Sinatra::Base
+  enable :sessions
+  set :session_secret, "My session secret"
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    session[:Player1_Name] = params[:Player1_Name]
+    session[:Player2_Name] = params[:Player2_Name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @Player1_Name = session[:Player1_Name]
+    @Player2_Name = session[:Player2_Name]
+    erb :play
+  end
+end
